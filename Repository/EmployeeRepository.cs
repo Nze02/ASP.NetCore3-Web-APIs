dad51d73c8c8c9cab56af93a2f34d3cab8d15ecc
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-06-29 09:53:29 +0100

    Converting Employee Repository and Controller to Asynchronous code

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Repository
 {
@@ -15,11 +16,11 @@ namespace Repository
             
         }
 
-        public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) =>
+        public async Task<IEnumerable<Employee>> GetEmployeesAsync(Guid companyId, bool trackChanges) =>
                 FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
                 .OrderBy(e => e.Name);
 
-        public Employee GetEmployee(Guid companyId, Guid id, bool trackChanges) =>
+        public async Task<Employee> GetEmployeeAsync(Guid companyId, Guid id, bool trackChanges) =>
             FindByCondition(e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges)
             .SingleOrDefault();
 
