77dcf5bee71e80f4fa86d118fec11ffabbfc9b19
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-07-01 15:22:42 +0100

    Implementing Filtering in the API (In EmployeeRepsitory and Get action)

--- ASP.NetCore3-Web-APIs/Controllers/EmployeesController.cs
+++ ASP.NetCore3-Web-APIs/Controllers/EmployeesController.cs
@@ -32,6 +32,9 @@ namespace ASP.NetCore3_Web_APIs.Controllers
         [HttpGet]
         public async Task<IActionResult> GetEmployeesForCompany(Guid companyId, [FromQuery] EmployeeParameters employeeParameters)
         {
+            if (!employeeParameters.ValidAgeRange)
+                return BadRequest("Max age can't be less than min age.");
+
             var company = await _repository.Company.GetCompanyAsync(companyId, trackChanges: false);
             if (company == null)
             {
