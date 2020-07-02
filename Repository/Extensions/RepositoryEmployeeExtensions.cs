6f004ca7d41d18329f1e96ca9bfb1843c2145813
Nzekwe <emmanuelnzekwe24@gmail.com>
2020-07-02 14:12:45 +0100

    Implementing Sorting in the API

--- Repository/Extensions/RepositoryEmployeeExtensions.cs
+++ Repository/Extensions/RepositoryEmployeeExtensions.cs
@@ -1,7 +1,9 @@
 ﻿using Entities.Models;
+using Repository.Extensions.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace Repository.Extensions
@@ -20,5 +22,23 @@ namespace Repository.Extensions
 
             return employees.Where(e => e.Name.ToLower().Contains(lowerCaseTerm));
         }
+
+        public static IQueryable<Employee> Sort(this IQueryable<Employee> employees, string orderByQueryString)
+        {
+            //basic check on the orderByQueryString
+            if (string.IsNullOrWhiteSpace(orderByQueryString))
+                return employees.OrderBy(e => e.Name);
+
+            
+            //removing excess commas and doing one last check to see if our query indeed has something in it
+            var orderQuery = OrderQueryBuilder.CreateOrderQuery<Employee>(orderByQueryString);
+
+            if (string.IsNullOrWhiteSpace(orderQuery))
+                return employees.OrderBy(e => e.Name);
+
+            return employees.OrderBy(e => e.Age).ThenByDescending(o => o.Name);    //short trick
+            //return employees.OrderBy(employees => orderQuery);
+        }
+
     }
 }
